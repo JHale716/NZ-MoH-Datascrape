ac5763e9aa3242c12e2c1c584124ac2ad0edbb31
J-P Hale <16615241+JHale716@users.noreply.github.com>
2021-12-25 15:25:05 +1300

    Sweeping Up - Update of MoH Data from local

---
+++ 2021-12-25_15-20-00-Loc/Support/js_fNgZ4Okisfr70Q0yvAYsATgB0o_jD3-srhQQ-VHmlXE.js
@@ -0,0 +1,283 @@
+/**
+ * @file
+ * CKEditor Accordion functionality overriden by custom js.
+ */
+
+(function ($) {
+  'use strict';
+  Drupal.behaviors.ckeditorAccordion = {
+    attach: function (context, settings) {
+
+      // Create accordion functionality if the required elements exist is available.
+      var $ckeditorAccordion = $('.ckeditor-accordion');
+      if ($ckeditorAccordion.length > 0) {
+        var c = 0;
+        // Create simple accordion mechanism for each tab.
+        $ckeditorAccordion.each(function () {
+          var $accordion = $(this);
+          if ($accordion.hasClass('styled')) {
+            return;
+          }
+
+          // The first one is the correct one.
+          if (typeof Drupal.settings.ckeditor_accordion == 'undefined' ||
+            (typeof Drupal.settings.ckeditor_accordion != 'undefined' && Drupal.settings.ckeditor_accordion.collapseAll != 1)) {
+            $accordion.children('dt a:first').addClass('active').attr('aria-expanded', 'true');
+            $accordion.children('dd:first').addClass('active').attr('aria-hidden', 'false');
+            $accordion.children('dd:first').css('display', 'block');
+          }
+
+          // Turn the accordion tabs to links so that the content is accessible & can be traversed using keyboard.
+          $accordion.children('dt').each(function () {
+            var $tab = $(this);
+            var id = 'moh-accordion-' + ++c;
+            $(this).next('dd').attr('id', id);
+            var tabText = $tab.text().trim();
+            var toggleClass = $tab.hasClass('active') ? ' active' : '';
+            $tab.html('<a class="ckeditor-accordion-toggler" href="#" aria-controls=' + id +' aria-expanded="false">' + tabText + '<span class="ckeditor-accordion-toggle' + toggleClass + '"></span></a>');
+          });
+
+          // Wrap the accordion in a div element so that quick edit function shows the source correctly.
+          $accordion.addClass('styled').removeClass('ckeditor-accordion').wrap('<div class="ckeditor-accordion-container"></div>');
+
+          // Fire an event so other modules know when the accordion has been created.
+          $accordion.trigger('ckeditorAccordionAttached');
+        });
+
+        // Add click event to body once because quick edits & ajax calls might reset the HTML.
+        $('body').once('ckeditorAccordionToggleEvent').on('click', '.ckeditor-accordion-toggler', function (e) {
+          var $t = $(this);
+          var $parent = $t.parent();
+
+          // Clicking on open element, close it.
+          if ($t.hasClass('active')) {
+            $t.removeClass('active').attr('aria-expanded', 'false');
+            $parent.removeClass('active');
+            $parent.next().slideUp();
+            $parent.parent().children('dd.active').attr('aria-hidden', 'true');
+          }
+          else {
+            // Remove active classes.
+            $parent.parent().children('dd.active').attr('aria-hidden', 'true').slideUp().removeClass('active');
+            $parent.parent().children('dt').children('a').attr('aria-expanded', 'false').removeClass('active');
+            $parent.parent().children('dt').removeClass('active');
+
+            // Show the selected tab.
+            $t.addClass('active').attr('aria-expanded', 'true');
+            $parent.addClass('active');
+            $parent.next().slideDown(300).addClass('active').attr('aria-hidden', 'false');
+          }
+
+          // Don't add hash to url.
+          e.preventDefault();
+        });
+      }
+    }
+  };
+})(jQuery);
+;
+/**
+ * @file
+ */
+
+(function ($) {
+
+  'use strict';
+
+  Drupal.extlink = Drupal.extlink || {};
+
+  Drupal.extlink.attach = function (context, settings) {
+    if (!settings.hasOwnProperty('extlink')) {
+      return;
+    }
+
+    // Strip the host name down, removing ports, subdomains, or www.
+    var pattern = /^(([^\/:]+?\.)*)([^\.:]{1,})((\.[a-z0-9]{1,253})*)(:[0-9]{1,5})?$/;
+    var host = window.location.host.replace(pattern, '$2$3');
+    var subdomain = window.location.host.replace(host, '');
+
+    // Determine what subdomains are considered internal.
+    var subdomains;
+    if (settings.extlink.extSubdomains) {
+      subdomains = '([^/]*\\.)?';
+    }
+    else if (subdomain === 'www.' || subdomain === '') {
+      subdomains = '(www\\.)?';
+    }
+    else {
+      subdomains = subdomain.replace('.', '\\.');
+    }
+
+    // Build regular expressions that define an internal link.
+    var internal_link = new RegExp('^https?://([^@]*@)?' + subdomains + host, 'i');
+
+    // Extra internal link matching.
+    var extInclude = false;
+    if (settings.extlink.extInclude) {
+      extInclude = new RegExp(settings.extlink.extInclude.replace(/\\/, '\\'), 'i');
+    }
+
+    // Extra external link matching.
+    var extExclude = false;
+    if (settings.extlink.extExclude) {
+      extExclude = new RegExp(settings.extlink.extExclude.replace(/\\/, '\\'), 'i');
+    }
+
+    // Extra external link CSS selector exclusion.
+    var extCssExclude = false;
+    if (settings.extlink.extCssExclude) {
+      extCssExclude = settings.extlink.extCssExclude;
+    }
+
+    // Extra external link CSS selector explicit.
+    var extCssExplicit = false;
+    if (settings.extlink.extCssExplicit) {
+      extCssExplicit = settings.extlink.extCssExplicit;
+    }
+
+    // Define the jQuery method (either 'append' or 'prepend') of placing the icon, defaults to 'append'.
+    var extIconPlacement = settings.extlink.extIconPlacement || 'append';
+
+    // Find all links which are NOT internal and begin with http as opposed
+    // to ftp://, javascript:, etc. other kinds of links.
+    // When operating on the 'this' variable, the host has been appended to
+    // all links by the browser, even local ones.
+    // In jQuery 1.1 and higher, we'd use a filter method here, but it is not
+    // available in jQuery 1.0 (Drupal 5 default).
+    var external_links = [];
+    var mailto_links = [];
+    $('a:not(.' + settings.extlink.extClass + ', .' + settings.extlink.mailtoClass + '), area:not(.' + settings.extlink.extClass + ', .' + settings.extlink.mailtoClass + ')', context).each(function (el) {
+      try {
+        var url = '';
+        if (typeof this.href == 'string') {
+          url = this.href.toLowerCase();
+        }
+        // Handle SVG links (xlink:href).
+        else if (typeof this.href == 'object') {
+          url = this.href.baseVal;
+        }
+        if (url.indexOf('http') === 0
+          && ((!url.match(internal_link) && !(extExclude && url.match(extExclude))) || (extInclude && url.match(extInclude)))
+          && !(extCssExclude && $(this).is(extCssExclude))
+          && !(extCssExclude && $(this).parents(extCssExclude).length > 0)
+          && !(extCssExplicit && $(this).parents(extCssExplicit).length < 1)) {
+          external_links.push(this);
+        }
+        // Do not include area tags with begin with mailto: (this prohibits
+        // icons from being added to image-maps).
+        else if (this.tagName !== 'AREA'
+          && url.indexOf('mailto:') === 0
+          && !(extCssExclude && $(this).parents(extCssExclude).length > 0)
+          && !(extCssExplicit && $(this).parents(extCssExplicit).length < 1)) {
+          mailto_links.push(this);
+        }
+      }
+      // IE7 throws errors often when dealing with irregular links, such as:
+      // <a href="node/10"></a> Empty tags.
+      // <a href="http://user:pass@example.com">example</a> User:pass syntax.
+      catch (error) {
+        return false;
+      }
+    });
+
+    if (settings.extlink.extClass) {
+      Drupal.extlink.applyClassAndSpan(external_links, settings.extlink.extClass, extIconPlacement);
+    }
+
+    if (settings.extlink.mailtoClass) {
+      Drupal.extlink.applyClassAndSpan(mailto_links, settings.extlink.mailtoClass, extIconPlacement);
+    }
+
+    if (settings.extlink.extTarget) {
+      // Apply the target attribute to all links.
+      $(external_links).attr('target', settings.extlink.extTarget);
+      // Add rel attributes noopener and noreferrer.
+      $(external_links).attr('rel', function (i, val) {
+        // If no rel attribute is present, create one with the values noopener and noreferrer.
+        if (val == null) {
+          return 'noopener noreferrer';
+        }
+        // Check to see if rel contains noopener or noreferrer. Add what doesn't exist.
+        if (val.indexOf('noopener') > -1 || val.indexOf('noreferrer') > -1) {
+          if (val.indexOf('noopener') === -1) {
+            return val + ' noopener';
+          }
+          if (val.indexOf('noreferrer') === -1) {
+            return val + ' noreferrer';
+          }
+          // Both noopener and noreferrer exist. Nothing needs to be added.
+          else {
+            return val;
+          }
+        }
+        // Else, append noopener and noreferrer to val.
+        else {
+          return val + ' noopener noreferrer';
+        }
+      });
+    }
+
+    Drupal.extlink = Drupal.extlink || {};
+
+    // Set up default click function for the external links popup. This should be
+    // overridden by modules wanting to alter the popup.
+    Drupal.extlink.popupClickHandler = Drupal.extlink.popupClickHandler || function () {
+      if (settings.extlink.extAlert) {
+        return confirm(settings.extlink.extAlertText);
+      }
+    };
+
+    $(external_links).click(function (e) {
+      return Drupal.extlink.popupClickHandler(e, this);
+    });
+  };
+
+  /**
+   * Apply a class and a trailing <span> to all links not containing images.
+   *
+   * @param {object[]} links
+   *   An array of DOM elements representing the links.
+   * @param {string} class_name
+   *   The class to apply to the links.
+   * @param {string} icon_placement
+   *   'append' or 'prepend' the icon to the link.
+   */
+  Drupal.extlink.applyClassAndSpan = function (links, class_name, icon_placement) {
+    var $links_to_process;
+    if (Drupal.settings.extlink.extImgClass) {
+      $links_to_process = $(links);
+    }
+    else {
+      var links_with_images = $(links).find('img').parents('a');
+      $links_to_process = $(links).not(links_with_images);
+    }
+    $links_to_process.addClass(class_name);
+    var i;
+    var length = $links_to_process.length;
+    for (i = 0; i < length; i++) {
+      var $link = $($links_to_process[i]);
+      if ($link.css('display') === 'inline' || $link.css('display') === 'inline-block') {
+        if (class_name === Drupal.settings.extlink.mailtoClass) {
+          $link[icon_placement]('<span class="' + class_name + '" aria-label="' + Drupal.settings.extlink.mailtoLabel + '"></span>');
+        }
+        else {
+          $link[icon_placement]('<span class="' + class_name + '" aria-label="' + Drupal.settings.extlink.extLabel + '"></span>');
+        }
+      }
+    }
+  };
+
+  Drupal.behaviors.extlink = Drupal.behaviors.extlink || {};
+  Drupal.behaviors.extlink.attach = function (context, settings) {
+    // Backwards compatibility, for the benefit of modules overriding extlink
+    // functionality by defining an "extlinkAttach" global function.
+    if (typeof extlinkAttach === 'function') {
+      extlinkAttach(context);
+    }
+    else {
+      Drupal.extlink.attach(context, settings);
+    }
+  };
+
+})(jQuery);
+;
